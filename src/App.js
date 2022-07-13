8e943bccc61c7fb95412ab3b62aba7149540f8cb
dariaddz <dariadz@gmail.com>
2022-07-13 20:28:41 +0300

    app & index.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,66 @@
+import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+// import logo from "./logo.svg";
 import './App.css';
+// import DailyCaloriesForm from './components/DailyCaloriesForm';
+import MainPage from './components/mainPage';
+
+// import Layout  from './components/Layout';
+
+// import PrivateRoute from './components/PrivateRoute';
+// import PublicRoute from './components/PublicRoute';
+
+const HomePage = lazy(() => import('./pages/homePage'));
+// const RegistrationPage = lazy(() => import('./pages/registrationPage'));
+const LoginPage = lazy(() => import('./pages/loginPage'));
+// const CalculatorPage = lazy(() => import('./pages/calculatorPage'));
+// const DiaryPage = lazy(() => import('./pages/diaryPage/diaryPage'));
 
 function App() {
-  return <div className="App">start it better again</div>;
+  return (
+    <>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {/* <Route path="/" element={<MainPage />}> */}
+          {/* <Route path="/" element={<PublicRoute />}> */}
+          {/* <Route index element={<HomePage />} /> */}
+
+          {/* <Route
+            path="/register"
+            element={<PublicRoute restricted redirectTo="/" />}
+          > */}
+          {/* <Route path="register" element={<RegistrationPage />} /> */}
+          {/* </Route> */}
+          {/* <Route
+            path="/login"
+            element={<PublicRoute restricted redirectTo="/" />}
+          > */}
+
+          {/* <Route path="/login" element={<LoginPage />} /> */}
+          {/* </Route> */}
+          {/* <Route
+
+            path="/calculator"
+            element={<PrivateRoute redirectTo="/login" />}
+          > */}
+          {/* <Route path="/diary" element={<DiaryPage />} /> */}
+          {/* </Route> */}
+          {/* <Route
+
+            path="*"
+            element={<PublicRoute restricted redirectTo="/diary" />}
+          >
+            <Route path="*" element={<NotFoundPage />} />
+          </Route> */}
+          {/* </Route> */}
+          {/* </Route>
+          </Route> */}
+
+          {/* <Route path="/diary" element={<DiaryPage />} /> */}
+        </Routes>
+      </Suspense>
+    </>
+  );
 }
 
 export default App;
