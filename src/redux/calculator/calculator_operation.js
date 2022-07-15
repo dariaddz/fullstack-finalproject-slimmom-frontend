a3eb057f1a036f16be9afeae57c13b4b30cbfe98
SergiyenkoDY <85953081+lokomativniy@users.noreply.github.com>
2022-07-16 01:44:25 +0300

    add redax

--- src/redux/calculator/calculator_operation.js
+++ src/redux/calculator/calculator_operation.js
@@ -1,44 +1,32 @@
 import axios from 'axios';
-import {
-  calculatorRequest,
-  calculatorSuccess,
-  calculatorError,
-} from './calculatorulator_action';
-import { dateEatenProduct } from '../day/day_operations';
-
+import { calcRequest, calcSuccess, calcError } from './calculator-action';
+import { dateEatenProduct } from '../product/product-operations';
 const currentDate = new Date().toLocaleDateString('fr-CA');
 
-const apiPublic = axios.create({
-  baseURL: 'https://slim-mom-project.herokuapp.com/api/users/public',
-});
-
-const apiPrivate = axios.create({
-  baseURL: 'https://slim-mom-project.herokuapp.com/api/users/private',
-});
-
-export const calculatorDataPublic = calculatorFormParams => async dispatch => {
+export const calcData = calcFormParams => async dispatch => {
   try {
-    dispatch(calculatorRequest());
-    const { data } = await axios.post(apiPublic, calculatorFormParams);
-    localStorage.setItem(
-      'calculatorFormParams',
-      JSON.stringify(calculatorFormParams)
+    dispatch(calcRequest());
+    const { data } = await axios.post(
+      'https://slim-mom-project.herokuapp.com/api/users/public',
+      calcFormParams
     );
-    dispatch(calculatorSuccess(data));
+    localStorage.setItem('calcFormParams', JSON.stringify(calcFormParams));
+    dispatch(calcSuccess(data));
   } catch (error) {
-    dispatch(calculatorError(error.message));
+    dispatch(calcError(error.message));
   }
 };
 
-export const calculatorDataPrivate =
-  (calculatorFormParams, token) => async dispatch => {
-    try {
-      const { data } = await axios.post(apiPrivate, calculatorFormParams, {
-        headers: { Authorization: `Bearer ${token}` },
-      });
-      dispatch(calculatorSuccess(data));
-      dispatch(dateEatenProduct(currentDate));
-    } catch (error) {
-      dispatch(calculatorError(error.message));
-    }
-  };
+export const calcDataPrivate = (calcFormParams, token) => async dispatch => {
+  try {
+    const { data } = await axios.post(
+      'https://slim-mom-project.herokuapp.com/api/users/private',
+      calcFormParams,
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
+    dispatch(calcSuccess(data));
+    dispatch(dateEatenProduct(currentDate));
+  } catch (error) {
+    dispatch(calcError(error.message));
+  }
+};
--- src/redux/day/day_action.js
+++ src/redux/day/day_action.js
@@ -3,9 +3,27 @@ const addProductRequest = createAction('products/addProductRequest');
 const addProductSuccess = createAction('products/addProductSuccess');
 const addProductError = createAction('products/addProductError');
 
+const dateEatenProductsRequest = createAction(
+  'products/dateEatenProductsRequest'
+);
+const dateEatenProductsSuccess = createAction(
+  'products/dateEatenProductsSuccess'
+);
+const dateEatenProductsError = createAction('products/dateEatenProductsError');
+
+const deleteProductIdRequest = createAction('products/deleteProductIdRequest');
+const deleteProductIdSuccess = createAction('products/deleteProductIdSuccess');
+const deleteProductIdError = createAction('products/deleteProductIdError');
+
 const productActions = {
   addProductRequest,
   addProductSuccess,
   addProductError,
+  dateEatenProductsRequest,
+  dateEatenProductsSuccess,
+  dateEatenProductsError,
+  deleteProductIdRequest,
+  deleteProductIdSuccess,
+  deleteProductIdError,
 };
 export default productActions;
