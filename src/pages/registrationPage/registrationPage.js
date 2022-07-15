08bd157a4091aee97cd780d3dd0841034713d583
LenaCScheper <ven.lenysja@ukr.net>
2022-07-15 16:16:53 +0300

    добавила спинер и изменила пути

--- src/pages/registrationPage/registrationPage.js
+++ src/pages/registrationPage/registrationPage.js
@@ -1,7 +1,7 @@
+import { authSelectors } from '../../redux/auth';
 import { authOperations } from '../../redux/auth';
 import { useDispatch } from 'react-redux';
-// import { useNavigate } from 'react-router-dom';
-// import { toast } from 'react-hot-toast';
+import { useSelector } from 'react-redux';
 
 import { Box } from '@mui/material';
 import { RegistrationForm } from '../../components/registrationForm';
@@ -9,31 +9,24 @@ import { Spiner } from '../../components/spiner';
 
 export function RegistrationPage() {
   const dispatch = useDispatch();
-  // let navigate = useNavigate();
+  const isPending = useSelector(authSelectors.getIsPending);
 
   const onRegister = async ({ name, email, password }) => {
-    // const data = await
     dispatch(authOperations.register({ name, email, password }));
-    // .then(data => console.log('promis', data));
-    // console.log('registerpage', data);
   };
 
   return (
     <Box
       sx={{
-        // posution: { md: 'absolute' },
-        // top: { md: '240px', lg: '306px' },
-        // left: { md: '32px', lg: '16' },
         padding: {
           xs: '40px 20px 0 20px',
           md: '160px 32px 0 32px',
           lg: '160px 16px 0 16px',
         },
-        // margin: { xs: '40px 0 0 0', md: '160px 0 0 0' },
       }}
     >
       <RegistrationForm onRegister={onRegister} />
-      {/* <Spiner /> */}
+      {isPending && <Spiner />}
     </Box>
   );
 }
